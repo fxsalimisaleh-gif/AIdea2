c0c3ebc1b15244c5b5d11a87029ac703d61ef49c
saleh <fxsalimi.saleh@gmail.com>
2026-08-26 12:51:09 +0430

    Fix GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { LanguageProvider } from "./context/LanguageContext.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <LanguageProvider>
         <App />
       </LanguageProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
